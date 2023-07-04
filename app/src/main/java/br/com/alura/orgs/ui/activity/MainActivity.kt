ea39f67ac63f92b72c008d10146cd966964ba6d1
Gustavo Borges <ogusborges@gmail.com>
2023-07-03 22:22:43 -0300

    Adicionado Binding e Configuração Básica de RecyclerView no MainActivity

--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -2,11 +2,34 @@ package br.com.alura.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
+import br.com.alura.orgs.ui.adapter.ListaProdutosAdapter
+import br.com.alura.orgs.ui.model.ProdutoItem
 
 class MainActivity: Activity() {
+    private lateinit var listaProdutosRecyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listaProdutosRecyclerView = findViewById(R.id.listaProdutos)
+
+        listaProdutosRecyclerView.adapter = ListaProdutosAdapter(
+            this,
+            listOf(
+                ProdutoItem(nome = "Teste 1", descricao = "Teste 1", valor = 12.99),
+                ProdutoItem(nome = "Teste 2", descricao = "Teste 2", valor = 22.99),
+                ProdutoItem(nome = "Teste 3", descricao = "Teste 3", valor = 32.99),
+            )
+        )
+
+        listaProdutosRecyclerView.layoutManager = LinearLayoutManager(
+            this,
+            RecyclerView.VERTICAL,
+            false
+        )
     }
 }
