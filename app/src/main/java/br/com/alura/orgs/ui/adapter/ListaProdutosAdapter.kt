4e54f781da62b9dd772913969da8d78f29ca8076
Gustavo Borges <ogusborges@gmail.com>
2023-07-03 22:17:21 -0300

    Implementado Adapter de Lista de Produtos

--- app/src/main/java/br/com/alura/orgs/ui/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/br/com/alura/orgs/ui/adapter/ListaProdutosAdapter.kt
@@ -4,24 +4,37 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import br.com.alura.orgs.R
 import br.com.alura.orgs.ui.model.ProdutoItem
 
 class ListaProdutosAdapter(
     private val context: Context,
-    private val listaProdutos: ProdutoItem,
+    private val listaProdutos: List<ProdutoItem>,
 ): RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view)
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        fun bind(produtoItem: ProdutoItem) {
+            val nomeTextView = itemView.findViewById<TextView>(R.id.nome)
+            val descricaoTextView = itemView.findViewById<TextView>(R.id.descricao)
+            val valorTextView = itemView.findViewById<TextView>(R.id.valor)
+
+            nomeTextView.text = produtoItem.nome
+            descricaoTextView.text = produtoItem.descricao
+            valorTextView.text = produtoItem.valor.toString()
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.produto_item, parent, false)
+        )
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = listaProdutos.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val produto = listaProdutos[position]
+        holder.bind(produto)
     }
 }
