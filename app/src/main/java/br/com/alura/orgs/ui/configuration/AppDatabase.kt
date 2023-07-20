9015ac762bf994957bdaee3d2cdb1df5aea45a2c
Gustavo Borges <ogusborges@gmail.com>
2023-07-19 23:50:46 -0300

    Transformado classe AppDatabase em uma instância Singleton

--- app/src/main/java/br/com/alura/orgs/ui/configuration/AppDatabase.kt
+++ app/src/main/java/br/com/alura/orgs/ui/configuration/AppDatabase.kt
@@ -1,6 +1,8 @@
 package br.com.alura.orgs.ui.configuration
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import br.com.alura.orgs.ui.dao.ProdutoItemDAO
 import br.com.alura.orgs.ui.model.ProdutoItem
@@ -8,4 +10,14 @@ import br.com.alura.orgs.ui.model.ProdutoItem
 @Database(entities = [ProdutoItem::class], version = 1, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun produtoItemDao(): ProdutoItemDAO
+
+    companion object {
+        @Volatile private var database: AppDatabase? = null
+        private val name = "app_database"
+        fun getInstance(context: Context): AppDatabase = database ?:
+            Room.databaseBuilder(context, AppDatabase::class.java, name).build()
+                .also { database = it }
+
+    }
 }
+
